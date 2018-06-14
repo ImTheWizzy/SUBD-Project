2ac16ec4488cae95d4b0139092277a2f3568111a
martylery <jkata@mail.bg>
2018-06-14 23:34:47 +0300

    Recipe migration

--- db/migrate/20180611210523_create_recipes.rb
+++ db/migrate/20180611210523_create_recipes.rb
@@ -1,9 +1,6 @@
+require 'RawSQL.rb'
 class CreateRecipes < ActiveRecord::Migration[5.2]
-	def change 
-<<<<<<< HEAD
+	def change
 		RawSQL.new('../../lib/sql/recipe.sql').result(some_date: '2018-06-014 22:54:34')
-=======
-
->>>>>>> 0ab29c5dc667e9616d771a42c27a0dc29c28d147
 	end
 end
