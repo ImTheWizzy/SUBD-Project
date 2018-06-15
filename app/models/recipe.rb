1b5b0a21fd41e5c8abd534bca397dfe4ae6e2eb7
martylery <jkata@mail.bg>
2018-06-15 13:24:07 +0300

    AUTO INCREMENT ISSUE REPAIREDgit status!

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
-    def self.insert(id, user_id, name, category, description, body)
-        sql = sanitize_sql(['INSERT INTO recipes VALUES(?, ?, ?, ?, ?, ?)', id, user_id, name, category, description, body])
+    def self.insert(user_id, name, category, description, body)
+        sql = sanitize_sql(['INSERT INTO recipes VALUES(NULL, ?, ?, ?, ?, ?)', user_id, name, category, description, body])
         ActiveRecord::Base.connection.execute(sql)
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,6 @@ User.create(username: "User 1", image_url: "https://i0.wp.com/cu-sentry.com/wp-c
 User.create(username: "User 2", image_url: "https://i0.wp.com/cu-sentry.com/wp-content/uploads/2017/11/wu-tang.jpg", email: "aa@a.com", password: "123456", password_confirmation: "123456")
 User.create(username: "User 3", image_url: "https://i0.wp.com/cu-sentry.com/wp-content/uploads/2017/11/wu-tang.jpg", email: "aaa@a.com", password: "123456", password_confirmation: "123456")
 
-Recipe.insert(1, User.find(1).id, "Recipe 1", "Category 1", "Description 1", "Body 1")
-Recipe.insert(2, User.find(2).id, "Recipe 2", "Category 2", "Description 2", "Body 2")
-Recipe.insert(3, User.find(2).id, "Recipe 3", "Category 3", "Description 3", "Body 3")
+Recipe.insert(User.find(1).id, "Recipe 1", "Category 1", "Description 1", "Body 1")
+Recipe.insert(User.find(2).id, "Recipe 2", "Category 2", "Description 2", "Body 2")
+Recipe.insert(User.find(2).id, "Recipe 3", "Category 3", "Description 3", "Body 3")
